9d5d84964588bf792a9b47ad095d32e12b4ae25f
Janusz Lenar <malleor@gmail.com>
2022-06-25 19:04:02 +0200

    extracted challenge base class

---
+++ src/wordle/challenges/challenge.py
@@ -0,0 +1,17 @@
+class Challenge():
+    GOOD       = '🟩'
+    WRONG      = '⬜️'
+    DISLOCATED = '🟨'
+
+    def __init__(self):
+        pass
+
+    def _check_guess(self, word, guess):
+        def _check_letter(w, g):
+            if g is w:
+                return Challenge.GOOD
+            if g in self.word:
+                return Challenge.DISLOCATED
+            return Challenge.WRONG
+
+        return ''.join([_check_letter(w, g) for w, g in zip(word, guess)])
--- src/wordle/challenges/mock_challenge.py
+++ src/wordle/challenges/mock_challenge.py
@@ -1,17 +1,9 @@
-class MockChallenge():
-    GOOD       = '🟩'
-    WRONG      = '⬜️'
-    DISLOCATED = '🟨'
+from .challenge import Challenge
 
+class MockChallenge(Challenge):
     def __init__(self, word):
+        super().__init__()
         self.word = word
 
-    def check_word(self, guess):
-        def check_letter(w, g):
-            if g is w:
-                return MockChallenge.GOOD
-            if g in self.word:
-                return MockChallenge.DISLOCATED
-            return MockChallenge.WRONG
-
-        return ''.join([check_letter(w, g) for w, g in zip(self.word, guess)])
+    def check_guess(self, guess):
+        return super()._check_guess(self.word, guess)
